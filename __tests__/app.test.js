6a05c60903aa0884ebe3acbbfbf68171d3305be1
Chase Abbott <cabbott93@gmail.com>
2021-05-12 16:14:00 -0700

    TDD mungeReviews

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,8 @@ import app from '../lib/app.js';
 import supertest from 'supertest';
 import { weather } from '../data/weather.js'
 import { locations } from '../data/location.js'
-import { mungeWeather, mungeLocation } from '../lib/utils.js'
+import { reviews } from '../data/reviews.js'
+import { mungeWeather, mungeLocation, mungeReviews } from '../lib/utils.js'
 
 
 const request = supertest(app);
@@ -43,5 +44,16 @@ describe('API Routes', () => {
     expect(mungeLocation(locations)).toEqual(expected)
   })
 
+  it('It formats the reviews', () => {
+    const expected = [{
+      "name": "North India Restaurant",
+      "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/8713LkYA3USvWj9z4Yokjw/o.jpg",
+      "price": "$$",
+      "rating": 4.0,
+      "url": "https://www.yelp.com/biz/north-india-restaurant-san-francisco?adjust_creative=RI3U3_x5kvdAgzJ15R17Cw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=RI3U3_x5kvdAgzJ15R17Cw"
 
+    }]
+
+    expect(mungeReviews(reviews)).toEqual(expected)
+  })
 });
---
+++ data/reviews.js
@@ -0,0 +1,98 @@
+export const reviews = [{
+    "id": "gR9DTbKCvezQlqvD7_FzPw",
+    "alias": "north-india-restaurant-san-francisco",
+    "name": "North India Restaurant",
+    "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/8713LkYA3USvWj9z4Yokjw/o.jpg",
+    "is_claimed": true,
+    "is_closed": false,
+    "url": "https://www.yelp.com/biz/north-india-restaurant-san-francisco?adjust_creative=RI3U3_x5kvdAgzJ15R17Cw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=RI3U3_x5kvdAgzJ15R17Cw",
+    "phone": "+14153481234",
+    "display_phone": "(415) 348-1234",
+    "review_count": 1782,
+    "categories": [
+        {
+            "alias": "indpak",
+            "title": "Indian"
+        }
+    ],
+    "rating": 4.0,
+    "location": {
+        "address1": "123 Second St",
+        "address2": "",
+        "address3": "",
+        "city": "San Francisco",
+        "zip_code": "94105",
+        "country": "US",
+        "state": "CA",
+        "display_address": [
+            "123 Second St",
+            "San Francisco, CA 94105"
+        ],
+        "cross_streets": ""
+    },
+    "coordinates": {
+        "latitude": 37.787789124691,
+        "longitude": -122.399305736113
+    },
+    "photos": [
+        "https://s3-media4.fl.yelpcdn.com/bphoto/8713LkYA3USvWj9z4Yokjw/o.jpg",
+        "https://s3-media4.fl.yelpcdn.com/bphoto/W2oBBWPGm3bRYEuHKGMtCw/o.jpg",
+        "https://s3-media1.fl.yelpcdn.com/bphoto/YrS539sVhmyOZcowdJsP_Q/o.jpg"
+    ],
+    "price": "$$",
+    "hours": [
+        {
+            "open": [
+                {
+                    "is_overnight": false,
+                    "start": "1000",
+                    "end": "2300",
+                    "day": 0
+                },
+                {
+                    "is_overnight": false,
+                    "start": "1000",
+                    "end": "2300",
+                    "day": 1
+                },
+                {
+                    "is_overnight": false,
+                    "start": "1000",
+                    "end": "2300",
+                    "day": 2
+                },
+                {
+                    "is_overnight": false,
+                    "start": "1000",
+                    "end": "2300",
+                    "day": 3
+                },
+                {
+                    "is_overnight": false,
+                    "start": "1000",
+                    "end": "2330",
+                    "day": 4
+                },
+                {
+                    "is_overnight": false,
+                    "start": "1000",
+                    "end": "2330",
+                    "day": 5
+                },
+                {
+                    "is_overnight": false,
+                    "start": "1000",
+                    "end": "2300",
+                    "day": 6
+                }
+            ],
+            "hours_type": "REGULAR",
+            "is_open_now": true
+        }
+    ],
+    "transactions": [
+        "restaurant_reservation",
+        "delivery",
+        "pickup"
+    ]
+}]
