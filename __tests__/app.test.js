a8d128ec797d7c1557ce13ec36b78ce564b72b17
Chase Abbott <cabbott93@gmail.com>
2021-05-12 15:43:50 -0700

    location  api endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,8 @@
 import app from '../lib/app.js';
 import supertest from 'supertest';
 import { weather } from '../data/weather.js'
-import { mungeWeather } from '../lib/utils.js'
+import { locations } from '../data/location.js'
+import { mungeWeather, mungeLocation } from '../lib/utils.js'
 
 
 const request = supertest(app);
@@ -12,7 +13,7 @@ describe('API Routes', () => {
   // If a GET request is made to /api/cats, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it('It formats the weather', async () => {
+  it('It formats the weather', () => {
     // act - make the request
     const expected = [
       {
@@ -24,4 +25,21 @@ describe('API Routes', () => {
     expect(mungeWeather(weather)).toEqual(expected);
 
   });
+
+  it('It formats the location', () => {
+    const expected = [
+      {
+        "formatted_query": "Seattle, King County, Washington, USA",
+        "latitude": "47.6038321",
+        "longitude": "-122.3300624"
+      },
+      {
+        "formatted_query": "Seattle, Shota Rustaveli Street, Rakatboshi mahalla, Yakkasaray district, Tashkent, 100000, Uzbekistan",
+        "latitude": "41.2888524",
+        "longitude": "69.2563883"
+      }
+    ]
+
+    expect(mungeLocation(locations)).toEqual(expected)
+  })
 });
---
+++ data/location.js
@@ -0,0 +1,40 @@
+export const locations = [
+    {
+        "place_id": "235549103",
+        "licence": "https://locationiq.com/attribution",
+        "osm_type": "relation",
+        "osm_id": "237385",
+        "boundingbox": [
+            "47.4810022",
+            "47.7341357",
+            "-122.459696",
+            "-122.224433"
+        ],
+        "lat": "47.6038321",
+        "lon": "-122.3300624",
+        "display_name": "Seattle, King County, Washington, USA",
+        "class": "place",
+        "type": "city",
+        "importance": 0.782979173564379,
+        "icon": "https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png"
+    },
+    {
+        "place_id": "62052055",
+        "licence": "https://locationiq.com/attribution",
+        "osm_type": "node",
+        "osm_id": "5053661783",
+        "boundingbox": [
+            "41.2888024",
+            "41.2889024",
+            "69.2563383",
+            "69.2564383"
+        ],
+        "lat": "41.2888524",
+        "lon": "69.2563883",
+        "display_name": "Seattle, Shota Rustaveli Street, Rakatboshi mahalla, Yakkasaray district, Tashkent, 100000, Uzbekistan",
+        "class": "amenity",
+        "type": "cafe",
+        "importance": 0.11100000000000002,
+        "icon": "https://locationiq.org/static/images/mapicons/food_cafe.p.20.png"
+    }
+]
